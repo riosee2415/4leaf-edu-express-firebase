18bfb862dc5710fb04c1f55130e76b46f9b64dba
4leaf.ysh <4leaf.ysh@gmail.com>
2020-06-13 15:42:31 +0900

    20200613

---
+++ academy_client/src/screens/ACSC04.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ACSC04 extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>ACSC04</div>
+      </div>
+    );
+  }
+}
+
+export default ACSC04;
